a60faf5d27a9c68abb8d613f929fc27c67cf41df
akitsuki <navarreitor86@gmail.com>
2018-03-06 17:47:21 +0100

    Revert "4_Navarro_añadirTasca"
    This reverts commit d5ccc198991dd6221382fdaeaa662e22a33150b4.

---
+++ var/cache/dev/twig/5f/5f52e6dc5c4d250a4f8233dee133387cc132a3e5627bcc9e208c3f8a92357c06.php
@@ -0,0 +1,58 @@
+<?php
+
+/* @Twig/images/chevron-right.svg */
+class __TwigTemplate_753c45dd6e14e1d5312b736904f85c2daebc970e0ccc6b194b70f46949871456 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_d9f7ecb9f99dea6eed70a58ce19aca35c7717bd86197b68f85fd272048069bdf = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_d9f7ecb9f99dea6eed70a58ce19aca35c7717bd86197b68f85fd272048069bdf->enter($__internal_d9f7ecb9f99dea6eed70a58ce19aca35c7717bd86197b68f85fd272048069bdf_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Twig/images/chevron-right.svg"));
+
+        $__internal_5d0078f05c10acec1b4ffffb4949843f624cf805951ac2be22a7412bc8a78301 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_5d0078f05c10acec1b4ffffb4949843f624cf805951ac2be22a7412bc8a78301->enter($__internal_5d0078f05c10acec1b4ffffb4949843f624cf805951ac2be22a7412bc8a78301_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@Twig/images/chevron-right.svg"));
+
+        // line 1
+        echo "<svg width=\"1792\" height=\"1792\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#FFF\" d=\"M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z\"/></svg>
+";
+        
+        $__internal_d9f7ecb9f99dea6eed70a58ce19aca35c7717bd86197b68f85fd272048069bdf->leave($__internal_d9f7ecb9f99dea6eed70a58ce19aca35c7717bd86197b68f85fd272048069bdf_prof);
+
+        
+        $__internal_5d0078f05c10acec1b4ffffb4949843f624cf805951ac2be22a7412bc8a78301->leave($__internal_5d0078f05c10acec1b4ffffb4949843f624cf805951ac2be22a7412bc8a78301_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Twig/images/chevron-right.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  25 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<svg width=\"1792\" height=\"1792\" viewBox=\"0 0 1792 1792\" xmlns=\"http://www.w3.org/2000/svg\"><path fill=\"#FFF\" d=\"M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z\"/></svg>
+", "@Twig/images/chevron-right.svg", "C:\\xampp\\htdocs\\symfony\\vendor\\symfony\\symfony\\src\\Symfony\\Bundle\\TwigBundle\\Resources\\views\\images\\chevron-right.svg");
+    }
+}
---
+++ var/cache/dev/twig/d4/d4339e07b38d691853e20298e8d9eff88f7b6f2839d2decfc70e03dfc6148791.php
@@ -0,0 +1,72 @@
+<?php
+
+/* PracticaBundle:Default:formtasca.html.twig */
+class __TwigTemplate_f66a2ba857e7a2bbab54679cb103a6a205c9ac684c83b37b3f34c9bb3c7529f1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_b3f03bab5527600fa922a3d68b5abcf86d41351fdfd641ffec7ec9288998d487 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_b3f03bab5527600fa922a3d68b5abcf86d41351fdfd641ffec7ec9288998d487->enter($__internal_b3f03bab5527600fa922a3d68b5abcf86d41351fdfd641ffec7ec9288998d487_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "PracticaBundle:Default:formtasca.html.twig"));
+
+        $__internal_c5a71d60e5daf4126d126d9b07153814f1e37b1a612e26cc517ad75ee3e49577 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_c5a71d60e5daf4126d126d9b07153814f1e37b1a612e26cc517ad75ee3e49577->enter($__internal_c5a71d60e5daf4126d126d9b07153814f1e37b1a612e26cc517ad75ee3e49577_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "PracticaBundle:Default:formtasca.html.twig"));
+
+        // line 1
+        echo "Formulario Tasca
+
+";
+        // line 3
+        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) ? $context["form"] : $this->getContext($context, "form")), 'form_start', array("action" => "", "method" => "POST"));
+        echo "
+";
+        // line 4
+        echo         $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->renderBlock((isset($context["form"]) ? $context["form"] : $this->getContext($context, "form")), 'form_end');
+        
+        $__internal_b3f03bab5527600fa922a3d68b5abcf86d41351fdfd641ffec7ec9288998d487->leave($__internal_b3f03bab5527600fa922a3d68b5abcf86d41351fdfd641ffec7ec9288998d487_prof);
+
+        
+        $__internal_c5a71d60e5daf4126d126d9b07153814f1e37b1a612e26cc517ad75ee3e49577->leave($__internal_c5a71d60e5daf4126d126d9b07153814f1e37b1a612e26cc517ad75ee3e49577_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "PracticaBundle:Default:formtasca.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  33 => 4,  29 => 3,  25 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("Formulario Tasca
+
+{{form_start(form,{'action':'','method':'POST'})}}
+{{form_end(form)}}", "PracticaBundle:Default:formtasca.html.twig", "C:\\xampp\\htdocs\\symfony\\src\\PracticaBundle/Resources/views/Default/formtasca.html.twig");
+    }
+}
